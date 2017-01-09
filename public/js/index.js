ca0ae37822d8e7d7eb395a1deb573e588e6112c8
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-01-09 14:23:19 +0200

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    to: 'asd@asd.asd',
-    text: 'create'
-  });
 });
 
 socket.on('disconnect', function() {
