f2ab674929c70a9485e18b8e8acfaf33015e9c50
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-01-10 10:45:49 +0200

    add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,18 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('New message sent', message);
+  var li = $('<li></li>');
+  li.text(message.from + ': ' + message.text);
+  $('#messages').append(li);
+});
+
+$('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function (text) {
+
+    console.log('got it, callback', text);
+  });
 });
