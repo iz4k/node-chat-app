d3bf66a98ff7f2bc1321bb6e8ed931e90197ecd2
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-01-10 12:51:20 +0200

    add geolocation support via goelocation api

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,16 @@ socket.on('newMessage', function(message) {
   $('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+  console.log('New location message sent', message);
+  var li = $('<li></li>');
+  var a = $('<a target="_blank">My current location</a>')
+  li.text(message.from + ': ');
+  a.attr('href', message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
+
 $('#message-form').on('submit', function (e) {
   e.preventDefault();
   socket.emit('createMessage', {
@@ -23,5 +33,22 @@ $('#message-form').on('submit', function (e) {
   }, function (text) {
 
     console.log('got it, callback', text);
+  });
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', function() {
+  if(!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser')
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function (err) {
+    console.log('Unable to fetch location');
+    console.log(err);
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 
 describe('generateMessage', () => {
@@ -7,6 +7,15 @@ describe('generateMessage', () => {
     var msg = generateMessage('ilari', 'test');
     expect(msg.from).toBe('ilari');
     expect(msg.text).toBe('test');
+    expect(msg.createdAt).toExist();
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var msg = generateLocationMessage('ilari', 25, 20);
+    expect(msg.from).toBe('ilari');
+    expect(msg.url).toBe('https://www.google.com/maps?q=25,20');
     expect(msg.createdAt).toExist();
   });
 });
