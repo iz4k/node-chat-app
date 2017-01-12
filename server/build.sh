f18f7b82724451a065ed64d3d6c4121f0f1eefc6
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-01-12 12:17:53 +0200

    build scripts for devops things

---
+++ server/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+####
+if (grep -i install <<< $@); then
+    #rm -rf test-bin
+    #mkdir test-bin
+    #echo 'Test report: Hello, world.' >test-bin/test-report.txt
+    echo "Install script"
+fi
+
+####
+if (grep -i test <<< $@); then
+    #rm -rf test-bin
+    #mkdir test-bin
+    #echo 'Test report: Hello, world.' >test-bin/test-report.txt
+    echo "Successfully ran all tests!"
+fi
+
+####
+if (grep -i build <<< $@); then
+    #echo "Looking for test report..."
+    #echo "Found it!"
+    #cat test-bin/test-report.txt
+    echo "Build script"
+fi
